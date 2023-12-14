8a8fc388df5ebe369efbfd002c94acd774406030
mayarrrrrr <akokmayar607@gmail.com>
2023-12-14 23:19:35 +0300

    Completed the assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function(){
+  
+    const result = 2000 / 100
+    return result
+}
+
+const square = (num1) => num1 * num1;
+square(8)
+
+const add = (num2 , num3) =>num2 +num3;
+add(3,4)
